441a04e485e7da8967befdcb82ad944980fa47a9
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-24 17:10:42 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LearningDootNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Build Adventures");
+            Console.ReadLine();
+        }
+    }
+}
