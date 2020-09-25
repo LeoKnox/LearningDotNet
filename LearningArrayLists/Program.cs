ff8e68db61493645eabff1b40b5d83ce711343de
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-25 16:49:20 -0700

    Add files via upload

--- LearningArrayLists/Program.cs
+++ LearningArrayLists/Program.cs
@@ -1,1 +1,22 @@
-a
+﻿using System;
+using System.Collections;
+
+namespace LearningArrayLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            list.Add("new string");
+            int i = 123;
+            object o = i;
+            i = 15; // does not change o
+            o = 10;
+            i = (int)o; // int required to cast o into i
+            Console.WriteLine(o);
+            Console.WriteLine((string)list[0]);
+            Console.ReadLine();
+        }
+    }
+}
