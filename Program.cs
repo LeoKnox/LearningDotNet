246b0d61e6be1f4af531db7647a20255830be4aa
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-24 17:16:36 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LearningDootNet
 {
@@ -6,7 +7,17 @@ namespace LearningDootNet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Build Adventures");
+            List<string> adventurers = new List<string>();
+            adventurers.Add("Aelien");
+            adventurers.Add("Eveehi");
+            adventurers.Add("Xingu");
+
+            Console.WriteLine(adventurers.Count);
+            foreach (var adventurer in adventurers)
+            {
+                Console.WriteLine(adventurer);
+            }
+            Console.WriteLine($" second adventurer is {adventurers[1]}");
             Console.ReadLine();
         }
     }
