57fadb0db355d60e2b000f7066bcc6c86af6dbf3
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-25 18:37:34 -0700

    Add files via upload

--- LearningHashTables/Program.cs
+++ LearningHashTables/Program.cs
@@ -1,1 +1,18 @@
-a
+﻿using System;
+using System.Collections;
+
+namespace LearningHashTables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable table = new Hashtable();
+            table.Add("title", "Website");
+
+            var s = (string)table["title"];
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
