5ff312f27dc11eaf75b89223801f3d051051f6df
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-26 21:55:54 -0700

    Add files via upload

--- LearningBitArray/Program.cs
+++ LearningBitArray/Program.cs
@@ -1,1 +1,22 @@
-a
+﻿using System;
+using System.Collections;
+
+namespace LearningBitArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[] preload = new bool[3] { true, false, true };
+            BitArray enemyGrid = new BitArray(preload);
+            enemyGrid[0] = false;
+            enemyGrid[1] = true;
+            enemyGrid.Set(2, false);
+            foreach (var enemy in enemyGrid)
+            {
+                Console.WriteLine(enemy);
+            }
+            Console.ReadLine();
+        }
+    }
+}
