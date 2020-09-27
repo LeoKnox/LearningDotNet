2a3ef5135531f4b45232b2266ef03210c0085192
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-26 22:43:19 -0700

    Add files via upload

--- LearningStack/Program.cs
+++ LearningStack/Program.cs
@@ -1,1 +1,24 @@
-a
+﻿using System;
+using System.Collections.Generic;
+
+namespace learningStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<String> pancakes = new Stack<string>();
+            pancakes.Push("blueberry");
+            pancakes.Push("chocolate");
+            pancakes.Push("bacon");
+            foreach(var pancake in pancakes)
+            {
+                Console.WriteLine(pancake);
+            }
+            Console.WriteLine(pancakes.Pop());
+            Console.WriteLine(pancakes.Peek());
+            Console.WriteLine(pancakes.Peek());
+            Console.ReadLine();
+        }
+    }
+}
