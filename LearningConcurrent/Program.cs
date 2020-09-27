e38b27ae3c72356ed37f61d7f8ddd707d0aac554
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-26 21:37:03 -0700

    Add files via upload

--- LearningConcurrent/Program.cs
+++ LearningConcurrent/Program.cs
@@ -1,1 +1,27 @@
-a
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace LearningConcurrent
+{
+    class Program
+    {
+        static ConcurrentDictionary<int, int> items = new ConcurrentDictionary<int, int>();
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(new ThreadStart(AddItem));
+            Thread thread2 = new Thread(new ThreadStart(AddItem));
+
+            thread1.Start();
+            thread2.Start();
+            Console.ReadLine();
+        }
+
+        static void AddItem()
+        {
+            items.TryAdd(1, 2);
+            Console.WriteLine(items.Count);
+        }
+    }
+}
