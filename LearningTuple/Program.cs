1e2d133f5bb8b12facbdd3dc108e83c11e386267
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-26 22:18:50 -0700

    Add files via upload

--- LearningTuple/Program.cs
+++ LearningTuple/Program.cs
@@ -1,1 +1,16 @@
-a
+﻿using System;
+
+namespace LearningTuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // tuple can be used to return items from function since traditionally only one item can be returned.
+            // Tuple<int, String, bool> myTuple = new Tuple<int, string, bool>(1, "atk", true);
+            var myTuple = Tuple.Create(1, "atk", true); // alternate way to create tuple cleaner and easier to implement
+            Console.WriteLine(myTuple.Item2);
+            Console.ReadLine();
+        }
+    }
+}
