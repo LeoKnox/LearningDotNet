2c2595fee31d60ae277d9df699a9e61788401bf5
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-27 20:24:34 -0700

    Add files via upload

--- LearningHashSet/Program.cs
+++ LearningHashSet/Program.cs
@@ -1,1 +1,22 @@
-a
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningHashSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myHash = new HashSet<string>();
+
+            myHash.Add("Hello");
+            myHash.Add("Hello");
+
+            Console.WriteLine(myHash.Count);
+
+            string[] s = new string[] { "Hello" };
+
+            Console.WriteLine(myHash.Overlaps(s));
+        }
+    }
+}
