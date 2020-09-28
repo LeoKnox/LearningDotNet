98ddd490a45f0cf1afde03d473478a8332b601fc
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-27 20:00:26 -0700

    Add files via upload

--- LearningQueue/Program.cs
+++ LearningQueue/Program.cs
@@ -1,1 +1,26 @@
-a
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> myQ = new Queue<int>();
+            myQ.Enqueue(1);
+            myQ.Enqueue(2);
+            myQ.Enqueue(3);
+            foreach (var q in myQ)
+            {
+                Console.WriteLine(q);
+            }
+            myQ.Dequeue();
+            foreach (var q in myQ)
+            {
+                Console.WriteLine(q);
+            }
+            Console.ReadLine();
+        }
+    }
+}
