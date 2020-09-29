fb721cd7aeb1e0c933dcacfd637dc515bc57b64e
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-28 18:20:52 -0700

    Add files via upload

--- LearningAsync/Program.cs
+++ LearningAsync/Program.cs
@@ -1,1 +1,25 @@
-a
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LearnAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Downloading file");
+            Download();
+            Console.ReadLine();
+        }
+
+        static void Download()
+        {
+            Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                Console.WriteLine("Downloading complete");
+            });
+        }
+    }
+}
