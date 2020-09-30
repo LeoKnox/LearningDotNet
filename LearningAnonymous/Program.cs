8d9ba06a2e3b43379d6d6b8760da19d8ccd767c5
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-29 18:05:19 -0700

    Add files via upload

--- LearningAnonymous/Program.cs
+++ LearningAnonymous/Program.cs
@@ -1,1 +1,30 @@
-a
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LearningAsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Downloading file");
+            Download();
+            Console.ReadLine();
+        }
+        static void Download()
+        {
+            Network.Download((message) => Console.WriteLine($"Download {message}"));
+        }
+        class Network
+        {
+            static public Task Download(Action<String> callback)
+            {
+                return Task.Run(() => {
+                    Thread.Sleep(3000);
+                    callback("completed");
+                });
+            }
+        }
+    }
+}
