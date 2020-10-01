fbacc4cd36218e7a6f183e7b316fccc5cfbb79eb
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-30 18:54:03 -0700

    Add files via upload

--- AutoImplementedProperties/Program.cs
+++ AutoImplementedProperties/Program.cs
@@ -1,1 +1,32 @@
-a
+﻿using System;
+
+namespace LearningProperties
+{
+    class Program
+    {
+        class Player
+        {
+            public bool IsAlive // uppercase i for IsAlive because it is a property
+            {
+                get { return Health > 0; } // properties allow adding check in getters and setter.
+            }
+
+            public int Health { get; set; } = 100;
+
+            public void Hit()
+            {
+                Random r = new Random();
+                Health -= r.Next(5, 50);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            for (int i = 0; i < 20; i++)
+            {
+                player.Hit();
+                Console.WriteLine($"Is player alive : {player.IsAlive}");
+            }
+        }
+    }
+}
