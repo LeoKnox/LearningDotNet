667a909e260f198a30b5e476634eb3357ede2dd2
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-30 18:40:12 -0700

    Add files via upload

--- LearningProperties/Program.cs
+++ LearningProperties/Program.cs
@@ -1,1 +1,31 @@
-a
+﻿using System;
+
+namespace LearningProperties
+{
+    class Program
+    {
+        class Player
+        {
+            public bool IsAlive // uppercase i for IsAlive because it is a property
+            {
+                get { return health > 0; } // properties allow adding check in getters and setter.
+            }
+            int health = 100;
+
+            public void Hit()
+            {
+                Random r = new Random();
+                health -= r.Next(5, 50);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            for (int i = 0; i < 20; i++)
+            {
+                player.Hit();
+                Console.WriteLine($"Is player alive : {player.IsAlive}");
+            }
+        }
+    }
+}
