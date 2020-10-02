462f4d16f839b07b5a3b246849f3f62855b35726
LeoKnox <LeoKnox@users.noreply.github.com>
2020-10-01 18:32:35 -0700

    Add files via upload

--- LearningThreads/Program.cs
+++ LearningThreads/Program.cs
@@ -1,1 +1,26 @@
-a
+﻿using System;
+using System.Threading;
+
+namespace LearningThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread mythread = new Thread(new ThreadStart(Work));
+                mythread.Start();
+            }
+
+            Console.ReadLine();
+        }
+
+        static void Work()
+        {
+            Console.WriteLine("Starting in thread: " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(2000);
+            Console.WriteLine("Doing work");
+        }
+    }
+}
