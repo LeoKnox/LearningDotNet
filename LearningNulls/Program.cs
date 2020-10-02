e299e91302930a616e8fe299a4e3e37893248808
LeoKnox <LeoKnox@users.noreply.github.com>
2020-10-01 18:53:24 -0700

    Add files via upload

--- LearningNulls/Program.cs
+++ LearningNulls/Program.cs
@@ -1,1 +1,15 @@
-a
+﻿using System;
+
+namespace LearningNulls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s1 = "hello";
+            var s2 = s1 ?? "default";
+
+            Console.WriteLine($"s2");
+        }
+    }
+}
