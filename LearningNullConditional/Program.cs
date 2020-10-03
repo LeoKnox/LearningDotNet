204c528eb210003d8f7e1dac3b0c7431f2dfbbc6
LeoKnox <LeoKnox@users.noreply.github.com>
2020-10-02 17:39:14 -0700

    Add files via upload

--- LearningNullConditional/Program.cs
+++ LearningNullConditional/Program.cs
@@ -1,1 +1,17 @@
-a
+﻿using System;
+
+namespace LearningNullConditional
+{
+    class Program
+    {
+        class Person
+        {
+            public string Name { get; set;  }
+        }
+        static void Main(string[] args)
+        {
+            Person person = null; // new Person() { Name = "Elric" }; // wht if this was null? would give errror
+            Console.WriteLine(person?.Name);
+        }
+    }
+}
